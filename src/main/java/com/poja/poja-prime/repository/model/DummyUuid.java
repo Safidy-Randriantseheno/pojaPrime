aecc8481338f82b421f7179b99368056ce1748ba
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-12-12 16:02:54 +0300

    feat: initiale project

---
+++ src/main/java/com/poja/poja-prime/repository/model/DummyUuid.java
@@ -0,0 +1,15 @@
+package com.poja.poja-prime.repository.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import com.poja.poja-prime.PojaGenerated;
+
+@PojaGenerated
+@Entity
+@Getter
+@Setter
+public class DummyUuid {
+  @Id private String id;
+}
